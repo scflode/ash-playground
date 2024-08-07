740169ab1e14699f72144109d7f7216434912ffb
Flo Schuessel <mail@florian-schuessel.de>
2024-08-07 16:42:53 +0200

    Update config

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :my_app, MyApp.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "my_app_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :my_app, MyApp.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "my_app_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
