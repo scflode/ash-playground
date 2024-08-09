f1e2ce57d04abfce3d7b5c9e4be58a0769097aab
Flo Schuessel <mail@florian-schuessel.de>
2024-08-09 12:47:48 +0200

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,34 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+
+tickets =
+  for i <- 0..5 do
+    ticket =
+      MyApp.Helpdesk.Support.Ticket
+      |> Ash.Changeset.for_create(:open, %{subject: "Issue #{i}"})
+      |> Ash.create!()
+
+    if rem(i, 2) == 0 do
+      ticket
+      |> Ash.Changeset.for_update(:close)
+      |> Ash.update!()
+    else
+      ticket
+    end
+  end
+
+representative =
+  MyApp.Helpdesk.Support.Representative
+  |> Ash.Changeset.for_create(:create, %{name: "Joe Armstrong"})
+  |> Ash.create!()
+
+ticket =
+  MyApp.Helpdesk.Support.Ticket
+  |> Ash.Changeset.for_create(:open, %{subject: "I can't find my hand!"})
+  |> Ash.create!()
+
+ticket
+|> Ash.Changeset.for_update(:assign, %{representative_id: representative.id})
+|> Ash.update!()
